b5f995ae311d8461e9e19d1d68dc0ec1e3d99325
BlockChyp SDK Builder <devops@blockchyp.com>
2024-05-06 15:12:15 -0600

    whoops

--- examples/ActivateTerminalExample.ts
+++ examples/ActivateTerminalExample.ts
@@ -1,6 +1,6 @@
-import BlockChyp from '@blockchyp/blockchyp-ts';
+import * as BlockChyp from '@blockchyp/blockchyp-ts';
 
-const client = BlockChyp.newClient({
+const client = BlockChyp.BlockChypClient.newClient({
   apiKey: 'ZDSMMZLGRPBPRTJUBTAFBYZ33Q',
   bearerToken: 'ZLBW5NR4U5PKD5PNP3ZP3OZS5U',
   signingKey: '9c6a5e8e763df1c9256e3d72bd7f53dfbd07312938131c75b3bfd254da787947'

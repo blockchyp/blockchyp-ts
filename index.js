7f8da9a1c3c6487c4fef60a68bfc7d62591de5fa
BlockChyp SDK Builder <devops@blockchyp.com>
2024-05-06 15:32:17 -0600

    moved index

--- index.js
+++ index.js
@@ -13,9 +13,12 @@ var __createBinding = (this && this.__createBinding) || (Object.create ? (functi
 var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.BlockChypClient = void 0;
 __exportStar(require("./src/client"), exports);
 var client_1 = require("./src/client");
-Object.defineProperty(exports, "BlockChypClient", { enumerable: true, get: function () { return client_1.default; } });
+Object.defineProperty(exports, "BlockChypClient", { enumerable: true, get: function () { return __importDefault(client_1).default; } });
 __exportStar(require("./src/models"), exports);

b90592446c64dbe860e955cf6686aaba39961716
Spencer Teeples <spencer.teeples@gmail.com>
2024-05-14 15:05:01 -0600

    returned default gateway host to prod

--- webpack.config.js
+++ webpack.config.js
@@ -30,8 +30,6 @@ var config = {
       "https": false,
       "stream": require.resolve('stream-browserify'),
       "crypto": require.resolve('crypto-browserify'),
-      "buffer": require.resolve('buffer'),
-      // "crypto-browserify": require.resolve('crypto-browserify'), //if you want to use this module also don't forget npm i crypto-browserify 
     }
   },
   devtool: 'source-map',

9ae5da5b1319a315726ee799c32d5ee7096602f6
Spencer Teeples <spencer.teeples@gmail.com>
2024-05-14 10:41:48 -0600

    updated fallbacks in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -28,8 +28,8 @@ var config = {
       "zlib": false,
       "http": false,
       "https": false,
-      "stream": false,
-      "crypto": false,
+      "stream": require.resolve('stream-browserify'),
+      "crypto": require.resolve('crypto-browserify'),
       "crypto-browserify": require.resolve('crypto-browserify'), //if you want to use this module also don't forget npm i crypto-browserify 
     }
   },

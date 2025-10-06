bb91ea9b90be8fc51aa25baffe6438bcfbbcce60
BlockChyp SDK Builder <devops@blockchyp.com>
2025-10-06 21:03:48 +0000

    Merge pull request #342 from blockchyp/task/VIP-473
    Task/vip 473 - Added the updateToken route to the SDKs

---
+++ examples/UpdateTokenExample.ts
@@ -0,0 +1,21 @@
+import * as BlockChyp from '@blockchyp/blockchyp-ts';
+
+const client = BlockChyp.newClient({
+  apiKey: 'ZDSMMZLGRPBPRTJUBTAFBYZ33Q',
+  bearerToken: 'ZLBW5NR4U5PKD5PNP3ZP3OZS5U',
+  signingKey: '9c6a5e8e763df1c9256e3d72bd7f53dfbd07312938131c75b3bfd254da787947'
+});
+
+const request = new BlockChyp.UpdateTokenRequest();
+request.token = '<TOKEN>';
+request.expMonth = 12;
+request.expYear = 2040;
+
+client.updateToken(request)
+.then(function(httpResponse) {
+    const response: BlockChyp.UpdateTokenResponse = httpResponse.data;
+    console.log('Response: ' + JSON.stringify(response));
+  })
+  .catch(function (error: any) {
+    console.log(error);
+  });
